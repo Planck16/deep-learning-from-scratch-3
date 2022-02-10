b963c31c05454cb0dd95bc6d6881f7f6a48f8462
Planck16 <Planck16@users.noreply.github.com>
2022-02-10 16:04:57 +0900

    add optimizer , sgd

---
+++ dezero/optimizers.py
@@ -0,0 +1,36 @@
+import math
+from dezero import cuda, Parameter
+
+
+class Optimizer:
+    def __init__(self):
+        self.target = None
+        self.hooks = []
+        
+    def setuup(self, target):
+        self.target = target
+        return self
+    
+    def update(self):
+        params = [p for p in self.target.params() if p.grad is not None]
+
+        for f in self.hooks:
+            f(params)
+        
+        for param in params:
+            self.update_one(param)
+        
+    def update_one(self, param):
+        raise NotImplementedError()
+    
+    def add_hook(self, f):
+        self.hooks.append(f)
+
+
+class SGD(Optimizer):
+    def __init__(self, lr=0.01):
+        super().__init__()
+        self.lr = lr
+    
+    def update_one(self, param):
+        param.data -= self.lr * param.grad.data
