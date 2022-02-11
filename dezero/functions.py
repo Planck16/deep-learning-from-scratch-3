9c4a26e2443ea487c13fab4e21624ec55b853d88
Planck16 <Planck16@users.noreply.github.com>
2022-02-11 17:23:42 +0900

    add log, clip, softmax_cross_entropy_simple

--- dezero/functions.py
+++ dezero/functions.py
@@ -63,6 +63,22 @@ class Exp(Function):
 def exp(x):
     return Exp()(x)
 
+
+class Log(Function):
+    def forward(self, x):
+        y = np.log(x)
+        return y
+    
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy / x
+        return gx
+
+
+def log(x):
+    return Log()(x)
+
+
 class Reshape(Function):
     def __init__(self, shape):
         self.shape = shape
@@ -307,3 +323,34 @@ class MeanSquaredError(Function):
 
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
+
+
+def softmax_cross_entropy_simple(x, t):
+    x, t = as_variable(x), as_variable(t)
+    N = x.shape[0]
+    p = softmax(x)
+    p = clip(p, 1e-15, 1.0)
+    log_p = log(p)
+    tlog_p = log_p[np.arange(N), t.data]
+    y = -1 * sum(tlog_p) / N
+    return y
+
+
+class Clip(Function):
+    def __init__(self, x_min, x_max):
+        self.x_min = x_min
+        self.x_max = x_max
+    
+    def forward(self, x):
+        y = np.clip(x, self.x_min, self.x_max)
+        return y
+    
+    def backward(self, gy):
+        x, = self.inputs
+        mask = (x.data >= self.x_min) * (x.data <= self.x_max)
+        gx = gy * mask
+        return gx
+
+
+def clip(x, x_min, x_max):
+    return Clip(x_min, x_max)(x)
