6cd5b6de1931c5ab59790fbca0ebbfbd368a76b8
Planck16 <Planck16@users.noreply.github.com>
2022-02-16 15:38:06 +0900

    add accuracy

--- dezero/functions.py
+++ dezero/functions.py
@@ -2,6 +2,7 @@ import numpy as np
 from dezero import utils
 from dezero.core import Function, Variable, as_variable, as_array
 
+
 class Sin(Function):
     def forward(self, x):
         y = np.sin(x)
@@ -334,6 +335,14 @@ def softmax_cross_entropy_simple(x, t):
     tlog_p = log_p[np.arange(N), t.data]
     y = -1 * sum(tlog_p) / N
     return y
+
+
+def accuracy(y, t):
+    y, t = as_variable(y), as_variable(t)
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
 
 
 class Clip(Function):
