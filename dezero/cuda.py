297848860c1cd145966dfa5f02a105df772c2fac
Planck16 <Planck16@users.noreply.github.com>
2022-02-16 18:46:39 +0900

    add cuda

--- dezero/cuda.py
+++ dezero/cuda.py
@@ -9,16 +9,9 @@ from dezero import Variable
 
 
 def get_array_module(x):
-    """Returns the array module for `x`.
-    Args:
-        x (dezero.Variable or numpy.ndarray or cupy.ndarray): Values to
-            determine whether NumPy or CuPy should be used.
-    Returns:
-        module: `cupy` or `numpy` is returned based on the argument.
-    """
     if isinstance(x, Variable):
         x = x.data
-
+    
     if not gpu_enable:
         return np
     xp = cp.get_array_module(x)
@@ -26,16 +19,9 @@ def get_array_module(x):
 
 
 def as_numpy(x):
-    """Convert to `numpy.ndarray`.
-    Args:
-        x (`numpy.ndarray` or `cupy.ndarray`): Arbitrary object that can be
-            converted to `numpy.ndarray`.
-    Returns:
-        `numpy.ndarray`: Converted array.
-    """
     if isinstance(x, Variable):
         x = x.data
-
+    
     if np.isscalar(x):
         return np.array(x)
     elif isinstance(x, np.ndarray):
@@ -44,16 +30,9 @@ def as_numpy(x):
 
 
 def as_cupy(x):
-    """Convert to `cupy.ndarray`.
-    Args:
-        x (`numpy.ndarray` or `cupy.ndarray`): Arbitrary object that can be
-            converted to `cupy.ndarray`.
-    Returns:
-        `cupy.ndarray`: Converted array.
-    """
     if isinstance(x, Variable):
         x = x.data
-
+    
     if not gpu_enable:
-        raise Exception('CuPy cannot be loaded. Install CuPy!')
+        raise Exception('CuPy cannot be loaded. Install CuPy')
     return cp.asarray(x)
